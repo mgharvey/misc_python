7674129235b828e8d4e8b5bea115fd6ddfb637bd
Michael G Harvey <mharve9@lsu.edu>
2014-08-04 22:25:53 -0500

    incorporate freebayes genotype calls

--- bin/freebayes_vcf2fa.py
+++ bin/freebayes_vcf2fa.py
@@ -11,9 +11,8 @@ Description: This script takes a vcf file output from freebayes (Garrison and Ma
 a program for calling genotypes and outputting information from bam files, and converts it
 to genotype sequences for use in e.g. coalescent modeling and phylogenetics. The input vcf
 must have been run with the freebayes option --report-monomorphic, which outputs all bases
-including invariant sites. This script does not use the freebayes genotype calls, but re-
-calls genotypes based solely on the read depth of each allele (any allele with read depth
-greater than the minimum coverage option set by the user is output). It is designed for 
+including invariant sites. This script uses the freebayes genotype calls, but filters out 
+alleles if they fall below a user-defined minimum coverage value. It is designed for 
 either haploid or diploid individuals (ploidy must be assigned in the options). Any loci 
 containing more alleles than the set ploidy level will be removed from the output (and are
 listed in a separate summary file). 
@@ -133,7 +132,7 @@ def main():
 			depth = int(match.group(1))
 			base = None 
 			if depth < args.min_cov: # Output n's for sites with low read depth
-				base = "n"*len(list(ref_allele))
+				base = "n"*len(list(ref_allele))			
 			else:
 				ref_allele = parts[3]
 				alt_allele = parts[4]
@@ -142,46 +141,65 @@ def main():
 				else:
 					alleles = list()
 					depths = list()
-					gt_info = parts[-1]
+					gt_info = parts[-1]					
 					info_parts = gt_info.split(":")
 					ref_depth = int(info_parts[2])
-					if ref_depth >= args.min_cov: # If sufficient reads for reference
-						alleles.append(ref_allele)
-						depths.append(ref_depth)
+							
+					# Get alleles and depths
+					alleles.append(ref_allele)
+					depths.append(ref_depth)
 					alt_depth = info_parts[4]
 					if "," not in alt_allele: # If only a single alt allele
-						if int(alt_depth) >= args.min_cov:
-							alleles.append(alt_allele)
-							depths.append(int(alt_depth))
+						alleles.append(alt_allele)
+						depths.append(int(alt_depth))
 					elif "," in alt_allele: # If multiple alt alleles
 						list_alt_alleles = alt_allele.split(",")
 						alt_depths = alt_depth.split(",")
 					 	for i, list_alt_allele in enumerate(list_alt_alleles):
-					 		if int(alt_depths[i]) >= args.min_cov:
-					 			alleles.append(list_alt_allele)
-					 			depths.append(int(alt_depths[i]))
-					if len(alleles) > 2:
+				 			alleles.append(list_alt_allele)
+				 			depths.append(int(alt_depths[i]))
+					
+					# Get genotypes and filter them by coverage
+					gt = info_parts[0]
+					gt_parts = gt.split("/")
+					gt_filtereds = list()
+					for gt_part in gt_parts:
+						if depths[int(gt_part)] >= args.min_cov:
+							gt_filtereds.append(gt_part)
+					gt_alleles = list()
+					gt_depths = list()
+					for gt_filtered in gt_filtereds:
+						gt_alleles.append(alleles[int(gt_filtered)])
+						gt_depths.append(depths[int(gt_filtered)])
+					gt_allele_set = list()
+					gt_allele_set = list(set(gt_alleles))
+
+					# Call bases
+					if len(gt_allele_set) > 2:
 						summary.write("{0}\n".format(name))
 						print "Locus {0} contains paralogous reads".format(name)
 						j += 1
-					elif len(alleles) == 2:
+					elif len(gt_allele_set) == 2:
 						if args.ploidy == 1:
 							summary.write("{0}\n".format(name))
 							print "Locus {0} contains paralogous reads".format(name)
 							j += 1							
 						elif args.ploidy == 2:
-							if len(list(alleles[0])) == len(list(alleles[1])) == 1:
-								base = unphase(alleles[0], alleles[1])
+							if len(gt_allele_set[0]) == len(gt_allele_set[1]) == 1:
+								base = unphase(gt_allele_set[0], gt_allele_set[1])
 							else:
-								if depths[0] > depths[1]:
-									base = alleles[0]
-								elif depths[1] > depths[0]:
-									base = alleles[1]
+								# Change this to be based on coverage?
+								gt_allele_set[0]
 							k += 1
-					elif len(alleles) == 1:
-						base = alleles[0]
-					elif len(alleles) == 0:
+							print gt_filtereds
+							print gt_alleles
+							print gt_depths
+							print base
+					elif len(gt_allele_set) == 1:
+						base = gt_alleles[0]
+					elif len(gt_allele_set) == 0:
 						base = "n"*len(list(ref_allele))
+					#print base
 			if base is not None:
 				seq.append(base)
 			prev_name = name
